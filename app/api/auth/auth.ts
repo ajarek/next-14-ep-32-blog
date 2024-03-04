738aeffea5565c4aab997763b2490e49a3b6b1ba
Ajarek <ajarek@poczta.onet.pl>
2024-03-04 21:44:01 +0100

    added authorize User

--- app/api/auth/auth.ts
+++ app/api/auth/auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GitHub from 'next-auth/providers/github'
-
+import { User } from '@/lib/models'
+import connectToDb from '@/lib/connectToDb'
+import { redirect } from 'next/dist/server/api-utils'
 export const {
   auth,
   handlers: { GET, POST },
@@ -14,17 +16,21 @@ export const {
     CredentialsProvider({
       name: 'Credential',
       credentials: {
-        username: {type:'text', required: true },
-        password: {type:'password', required: true },
+        username: { type: 'text', required: true },
+        password: { type: 'password', required: true },
       },
-      async authorize(credentials) {
-        const user ={id:'1',  username: 'admin', password: 'admin'}
-        if (credentials.username === user.username && credentials.password === user.password) {
-          return user;
-        } else {
-          return null;
+      async authorize(credentials: any) {
+        await connectToDb()
+        try {
+          const user = await User.findOne({ password: credentials.password })
+          if (user) {
+            return user
+          }
+          
+        } catch (err: any) {
+          throw new Error(err)
         }
-      }
+      },
     }),
   ],
   secret: process.env.AUTH_SECRET,
